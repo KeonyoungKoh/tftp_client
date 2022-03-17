38445f1c89aacbd0fb910ac400daf22098de9eab
KeonyoungKoh <kkyjusikhoisa@kaist.ac.kr>
2022-03-17 05:46:47 +0000

    added modularization

--- tftp_client_2.py
+++ tftp_client_2.py
@@ -1,42 +1,44 @@
+import time
 import zlib
 import struct
 import tftpy
 
-open_file_name = input('enter file name : ')
+def crc32():
+    open_file_name = input('enter file name : ')
 
-with open(open_file_name, "rb") as f:
-    data = f.read()
-f.close()
+    with open(open_file_name, "rb") as f:
+        data = f.read()
+    f.close()
+        
+    result = zlib.crc32(data) & 0xffffffff
+
+    newresult = struct.pack('<I', result)
+
+    send_file_name = input('enter new file name : ')
+
+    with open(send_file_name, "ab") as f2:
+        for i in range(128):
+            f2.write(newresult)
+        f2.write(data)
+    f2.close()
+
+    with open(send_file_name, "rb") as f3:
+        newdata = f3.read()
+    f3.close()
     
-result = zlib.crc32(data) & 0xffffffff
-print(type(result))
-print("file length : %d" % (len(data)))
-print("%08X" % (result))
+    return send_file_name
 
-print(struct.pack('<I', result))
+def send_update(host, filename):
+    print("opening tftp client...")
+    time.sleep(1)
+    print("updating...")
 
-newresult = struct.pack('<I', result)
-print(type(newresult))
+    client = tftpy.TftpClient(host, 69)
+    client.upload(filename, filename, timeout = 60)
 
-# packet for communication
-# 다른 정보 전송할 경우 첫 512B 패킷 사용해서 전달
+    print("update complete")
 
-send_file_name = input('enter new file name : ')
-
-with open(send_file_name, "ab") as f2:
-    for i in range(128):
-        f2.write(newresult)
-f2.close()
-
-with open(send_file_name, "ab") as f3:
-    f3.write(data)
-f3.close()
-
-with open(send_file_name, "rb") as f4:
-    newdata = f4.read()
-f4.close()
-
-print("file length : %d" % len(newdata))
-
-client = tftpy.TftpClient('192.168.0.181', 69)
-client.upload(send_file_name, send_file_name, timeout = 60)
+if __name__ == '__main__':
+    send_file_name = crc32()
+    send_update('192.168.0.181', send_file_name)
+    
